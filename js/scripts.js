5eb84e24cdf47e476297bcc0c19045bf79ee81fe
Joseumes <tunjose@umes.edu.gt>
2025-04-27 13:07:03 -0600

    creacion del login y la logica del js

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+document.getElementById('formulario-reserva').addEventListener('submit', function (event) {
+    event.preventDefault();
+    alert('Reserva realizada con éxito. ¡Gracias por elegirnos!');
+});
---
+++ login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio de SEsión - Hotel El Paraíso</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+    <div class="container d-flex justify-content-center align-items-center vh-100">
+        <div class="card p-4" style=" max-width: 400px;">
+            <h2 class="text-center mb-4"> Inicio de Sesión</h2>
+            <form action="dashboard.php" method="POST">
+                <div class="mb-3">
+                    <label for="usuario" class="form-label">Usuario</label>
+                    <input type="text" class="form-control" id="usuario" name="usuario" required>
+                </div>
+                <div class="mb-3">
+                    <label for="contrasena" class="form-label">Contraseña</label>
+                    <input type="password" class="form-control" id="contrasena" name="contrasena" required>
+                </div>
+                <button type="submit" class="btn btn-primary w-100"></button>
+            </form>
+            </div>
+    </div>
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+</html>
