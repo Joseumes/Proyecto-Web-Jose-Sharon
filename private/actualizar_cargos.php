93b90ef6eed0dd3702d0f432b2b694165a5e9a96
Joseumes <tunjose@umes.edu.gt>
2025-05-09 16:48:11 -0600

    Refactor code structure for improved readability and maintainability

--- private/actualizar_cargos.php
+++ private/actualizar_cargos.php
@@ -207,6 +207,7 @@ $habitacionesOcupadas = [
             </div>
         </div>
     </div>
+    
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
